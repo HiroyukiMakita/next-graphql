dcaae8609a17bf16dc491be96dbd88b9734c76e5
牧田紘幸 <hiroyukimakita.pc@gmail.com>
2022-04-24 15:47:03 +0900

    add: GraphQL 用のページ追加

---
+++ pages/graphql/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Layout } from '../../components/Layout';
+
+const GraphQL: NextPage = () => {
+  return (
+    <Layout>
+      <div>test</div>
+    </Layout>
+  );
+};
+
+export default GraphQL;
