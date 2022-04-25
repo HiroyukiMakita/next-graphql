af7e56583a959cf9937fe75f29b088226e56636b
牧田紘幸 <hiroyukimakita.pc@gmail.com>
2022-04-26 02:07:24 +0900

    add: faker、ts-node, @types/node をインストールして、シーダーを作成してシーディング

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { prisma } from './prismaClient';
+import { faker } from '@faker-js/faker';
+
+const main = async () => {
+  await prisma.product.createMany({
+    data: [...Array(10)].map((_) => {
+      return {
+        name: faker.name.findName(),
+        price: faker.random.alphaNumeric(50),
+        remarks: faker.random.words(),
+      };
+    }),
+  });
+};
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
