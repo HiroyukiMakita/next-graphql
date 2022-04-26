69c86d15a3bed14f4ae7dce929e0ac9ae130939e
牧田紘幸 <hiroyukimakita.pc@gmail.com>
2022-04-27 02:35:15 +0900

    add: 一覧表示を追加

--- pages/graphql/index.tsx
+++ pages/graphql/index.tsx
@@ -1,6 +1,9 @@
 import { gql, useQuery } from '@apollo/client';
 import type { NextPage } from 'next';
 import { Layout } from '../../components/Layout';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import dayjs from 'dayjs';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 const AllProductQuery = gql`
   query {
@@ -18,16 +21,92 @@ const AllProductQuery = gql`
 const GraphQL: NextPage = () => {
   const { data: products, loading: productsLoading } =
     useQuery(AllProductQuery);
+
+  const [innerWidth, updateInnerWidth] = useState(0);
+
+  useEffect(() => {
+    const onResize = () => {
+      updateInnerWidth(window.innerWidth);
+    };
+    window.onload = onResize;
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'id', description: '採番', width: 70 },
+    {
+      field: 'name',
+      headerName: 'name',
+      description: '名前',
+      width: innerWidth / 7,
+    },
+    {
+      field: 'price',
+      headerName: 'price',
+      description: '料金',
+      width: innerWidth / 7,
+    },
+    {
+      field: 'remarks',
+      headerName: 'remarks',
+      description: '備考',
+      width: innerWidth / 7,
+      sortable: false,
+    },
+    {
+      field: 'createdAt',
+      headerName: 'created_at',
+      description: '作成日時',
+      width: innerWidth / 7,
+    },
+    {
+      field: 'updatedAt',
+      headerName: 'updated_at',
+      description: '更新日時',
+      width: innerWidth / 7,
+    },
+    {
+      field: 'nameAndPrice',
+      headerName: 'name(price)',
+      description: 'name and price',
+      sortable: false,
+      width: innerWidth / 7,
+      valueGetter: (params: GridValueGetterParams) =>
+        `${params.row.name || ''} (${params.row.price || ''})`,
+    },
+  ];
+
   if (productsLoading) {
     return <div>loading</div>;
   }
+
+  const rows = products.products.map((product: any) => {
+    return {
+      id: product.id,
+      name: product.name,
+      price: `¥${Number(product.price).toLocaleString('ja')}`,
+      remarks: product.remarks,
+      createdAt: dayjs(Number(product.createdAt)).format('YYYY-MM-DD'),
+      updatedAt: dayjs(Number(product.updatedAt)).format('YYYY-MM-DD'),
+    };
+  });
+
   console.log('products: ', products);
   return (
-    <Layout>
-      {products.products.map((product: any) => (
-        <div key={product.id}>{product.name}</div>
-      ))}
-    </Layout>
+    <>
+      <Layout>
+        <h2>READ</h2>
+        <div style={{ height: 400, width: '80vw' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+          />
+        </div>
+      </Layout>
+    </>
   );
 };
 
